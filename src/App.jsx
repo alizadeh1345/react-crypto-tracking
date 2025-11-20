11cca1b7bf9f040816f9c8d5315fefeeff40ef9f
Hosseinalizadeh45 <hossein.alizadeh1966@gmail.com>
2025-11-20 10:27:32 +0330

    implement base structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,22 @@
+import FooterComponent from "./components/ui/Footer.component";
+import NavbarComponent from "./components/ui/Navbar.component";
 
 
 
 function App() {
   return (
-    <div className="App">
-      <h3>Alizadeh</h3>
+    <div >
+      {/* Navbar */}
+      <NavbarComponent/>
+      {/* Wrapper */}
+      <div className="wrapper">
+        <h2>
+          Wrapper
+        </h2>
+      </div>
+      {/* Footer */}
+      <FooterComponent/>
+
     </div>
   );
 }
