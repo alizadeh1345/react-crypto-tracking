77d178380d29ee3ae75bb192f51562c6f87d1e74
Hosseinalizadeh45 <hossein.alizadeh1966@gmail.com>
2025-11-20 11:33:04 +0330

    route implemented

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,10 @@
 import FooterComponent from "./components/ui/Footer.component";
 import NavbarComponent from "./components/ui/Navbar.component";
-
+import {Routes,Route} from 'react-router-dom'
+import HomePage from "./pages/Home.page";
+import FAQPage from "./pages/FAQ.page";
+import AboutUsPage from "./pages/AboutUs.page";
+import ContactUsPage from "./pages/ContactUs.page";
 
 
 function App() {
@@ -10,9 +14,12 @@ function App() {
       <NavbarComponent/>
       {/* Wrapper */}
       <div className="wrapper">
-        <h2>
-          Wrapper
-        </h2>
+        <Routes>
+          <Route path="/" element = {<HomePage/>}/>
+          <Route path="/faq" element = {<FAQPage/>}/>
+          <Route path="/about-us" element = {<AboutUsPage/>}/>
+          <Route path="/contact-us" element = {<ContactUsPage/>}/>
+        </Routes>
       </div>
       {/* Footer */}
       <FooterComponent/>
