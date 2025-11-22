69dfc292233974573e005b8f8ce188cddc6485ce
Hosseinalizadeh45 <hossein.alizadeh1966@gmail.com>
2025-11-22 21:09:57 +0330

    about and contact implement

--- src/pages/AboutUs.page.jsx
+++ src/pages/AboutUs.page.jsx
@@ -1,13 +1,34 @@
-import React from 'react';
+import React from "react";
+import aboutImg from "../assests/images/about.png";
 
 const AboutUsPage = () => {
-    return (
-        <div>
-        <h5>AboutUsPage</h5> 
-        <h5>AboutUsPage</h5>
-        <h5>AboutUsPage</h5>
+  return (
+    <div className="container">
+      <div className="row">
+        <div className="col-12 bg-info p-3 rounded">
+          <h3>About Us</h3>
         </div>
-    );
+      </div>
+      <div className="row align-items-center ">
+        <div className="col-md-7  ">
+          Le Lorem Ipsum est simplement du faux texte employé dans la
+          composition et la mise en page avant impression. Le Lorem Ipsum est le
+          faux texte standard de l'imprimerie depuis les années 1500, quand un
+          imprimeur anonyme assembla ensemble des morceaux de texte pour
+          réaliser un livre spécimen de polices de texte. Il n'a pas fait que
+          survivre cinq siècles, mais s'est aussi adapté à la bureautique
+          informatique, sans que son contenu n'en soit modifié. Il a été
+          popularisé dans les années 1960 grâce à la vente de feuilles Letraset
+          contenant des passages du Lorem Ipsum, et, plus récemment, par son
+          inclusion dans des applications de mise en page de texte, comme Aldus
+          PageMaker.
+        </div>
+        <div className="col-md-5  text-center">
+          <img src={aboutImg} alt="about" className="img-fluid about-img" />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default AboutUsPage;
--- src/pages/ContactUs.page.jsx
+++ src/pages/ContactUs.page.jsx
@@ -1,13 +1,50 @@
 import React from "react";
+import contactImg from "../assests/images/contact.png";
 
 const ContactUsPage = () => {
   return (
-    <div>
-      <h5>ContactUsPage</h5>
-      <h5>ContactUsPage</h5>
-      <h5>ContactUsPage</h5>
-      <h5>ContactUsPage</h5>
-      <h5>ContactUsPage</h5>
+    <div className="container">
+      <div className="row">
+        <div className="col-12 bg-info p-3 rounded">
+          <h3>Contact Us</h3>
+        </div>
+      </div>
+      <div className="row align-items-center ">
+        <div className="col-md-7  ">
+          <div className="mt-3">
+            <h3 className="text-primary">How To Contact Us</h3>
+            <h5>+98 915 532 1721</h5>
+            <h5>+51 5524 2111</h5>
+            <h5>test@test.com</h5>
+          </div>
+          <div className="mt-3">
+            <div className="mb-3">
+              <label  className="form-label">
+                Your Name
+              </label>
+              <input
+                type="email"
+                className="form-control"
+                placeholder="please  type your name"
+              />
+            </div>
+            <div className="mb-3">
+              <label  className="form-label">
+                Your Message
+              </label>
+              <input
+                type="email"
+                className="form-control"
+                placeholder="please  type your message"
+              />
+            </div>
+            <button className="btn btn-outline-primary ms-3">send</button>
+          </div>
+        </div>
+        <div className="col-md-5  text-center">
+          <img src={contactImg} alt="contact" className="img-fluid about-img" />
+        </div>
+      </div>
     </div>
   );
 };
