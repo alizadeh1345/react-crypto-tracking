c61073a0cba48ae414c9664507de43ba24f544c3
Hosseinalizadeh45 <hossein.alizadeh1966@gmail.com>
2025-11-25 18:17:54 +0330

    coinDetails implement

---
+++ src/pages/Coin.page.jsx
@@ -0,0 +1,43 @@
+import React, { useState } from "react";
+import { useEffect } from "react";
+import { useParams } from "react-router-dom";
+import { detailsUrl } from "../constants/api";
+import { baseUrl } from "../constants/api";
+import axios from "axios";
+import CoinDetailsComponent from "../components/coins/CoinDetails.component";
+import Spinner from "../components/ui/Spinner.component";
+
+const CoinPage = () => {
+  const paramObject = useParams();
+  const url = detailsUrl(paramObject.id);
+  const [coin, setCoin] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState("");
+
+  useEffect(() => {
+    setLoading(true);
+    axios(url)
+      .then((response) => {
+        setLoading(false);
+        // console.log(response.data);
+        setCoin(response.data);
+      })
+      .catch((error) => {
+        setError("An error occured");
+      });
+  }, []);
+
+  return (
+    <div className="container">
+      {loading ? (
+        <Spinner />
+      ) : error !== "" ? (
+        <h1>error</h1>
+      ) : (
+        <CoinDetailsComponent  coin={coin}/>
+      )}
+    </div>
+  );
+};
+
+export default CoinPage;
